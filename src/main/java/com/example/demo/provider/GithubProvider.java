60fcd03601456be72202f51e6c4b746d6911cb66
lihao1998 <1192263018@qq.com>
2019-07-22 15:58:51 +0800

    解决获取用户信息JSON解析失败错误

--- src/main/java/com/example/demo/provider/GithubProvider.java
+++ src/main/java/com/example/demo/provider/GithubProvider.java
@@ -48,11 +48,11 @@ public class GithubProvider {
     public GithubUser getUser(String accessToken){
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token="+accessToken)
+                .url("https://api.github.com/user?access_token=" + accessToken)
                 .build();
         try {
             Response response = client.newCall(request).execute();
-            String string = response.body().toString();
+            String string = response.body().string();
             System.out.println(string);
             GithubUser githubUser = JSON.parseObject(string,GithubUser.class);
             return githubUser;
