c3e21f50f2ce0941753efa1333eeebd6a61373ce
李浩 <leehao@lihaodeMacBook-Pro.local>
2019-07-24 15:04:38 +0800

    添加回答类型

--- src/main/java/com/example/demo/provider/GithubProvider.java
+++ src/main/java/com/example/demo/provider/GithubProvider.java
@@ -67,56 +67,86 @@ public class GithubProvider {
 
     /**
      * 用户问题回答
-     * @param userDcr,userQuestion
+     * @param userDcr,userQuestion,type 0-支持重复句式异常 1-剔除所有重复字符串
      * @return result
      */
-    public static String getAnswer(String userDcr,String userQuestion){
+    public static String getAnswer(String userDcr,String userQuestion,int type){
 //        Log.i("TAG","用户话语:" + userDcr + "," + "用户提问:" + userQuestion);
 //        Properties proper = ProperTies.getProperties(ApplicationUtil.getContext());
         int count = 0;//重复数
         String result = "";//结果字符串
 //        String exceptionText = proper.getProperty("exceptionText");
         char[] dcrArray = userDcr.toCharArray(),questionArray = userQuestion.toCharArray();
-        for(int i=0;i<dcrArray.length;i++){
-            for(int j=0;j<questionArray.length;j++){
-                if(dcrArray[i]==questionArray[j]){
-                    result += String.valueOf(dcrArray[i]);
-                    count++;
-                }
-            }
-        }
-        //如果有重复,否则抛出异常答复
-        if(count!=0){
-            count = 0;
-            //将重复字符串从用户话语中删除
-            result = userDcr.replace(result,"");
-            char[] resultArray = result.toCharArray();
-            for(int i=0;i<resultArray.length;i++){
-                for(int j=0;j<questionArray.length;j++){
-                    if(resultArray[i]==questionArray[j]){
-                        count++;
+        switch (type){
+            case 0:
+                for(int i=0;i<dcrArray.length;i++){
+                    for(int j=0;j<questionArray.length;j++){
+                        if(dcrArray[i]==questionArray[j]){
+                            result += String.valueOf(dcrArray[i]);
+                            count++;
+                        }
                     }
                 }
-            }
-            if(count!=0){
-                result = "哈哈换个方式问我吧";
+                //如果有重复,否则抛出异常答复
+                if(count!=0){
+                    count = 0;
+                    //将重复字符串从用户话语中删除
+                    result = userDcr.replace(result,"");
+                    char[] resultArray = result.toCharArray();
+                    for(int i=0;i<resultArray.length;i++){
+                        for(int j=0;j<questionArray.length;j++){
+                            if(resultArray[i]==questionArray[j]){
+                                count++;
+                            }
+                        }
+                    }
+                    if(count!=0){
+                        result = "哈哈换个方式问我吧";
 //                Log.w("TAG","语句多次重复,抛出异常回答:" + exceptionText);
-                System.out.println(result);
-            }
-            else{
+                        System.out.println(result);
+                    }
+                    else{
 //                Log.i("TAG","成功:" + result);
-                System.out.println(result);
-            }
-        }
-        else{
+                        System.out.println(result);
+                    }
+                }
+                else{
 //            Log.w("TAG","无重复部分,抛出异常回答:" + exceptionText);
-            result = "哈哈换个方式问我吧";
-            System.out.println(result);
+                    result = "哈哈换个方式问我吧";
+                    System.out.println(result);
+                }
+                return result;
+            case 1:
+                for(int i=0;i<dcrArray.length;i++){
+                    for(int j=0;j<questionArray.length;j++){
+                        if(dcrArray[i]==questionArray[j]){
+                            result += String.valueOf(dcrArray[i]);
+                            count++;
+
+                        }
+                    }
+                }
+                if(count==0){
+                    result = "哈哈换个方式问我吧";
+                    System.out.println(result);
+                }
+                else{
+                    char[] resultArray = result.toCharArray();
+                    //循环用户话语字符串，将重复的依次剔除
+                    for(int z=0;z<resultArray.length;z++){
+                        userDcr = userDcr.replace(String.valueOf(resultArray[z]),"");
+                    }
+                    result = userDcr;
+                }
+                System.out.println(result);
+                return result;
+                default:
+                    System.out.println("输入type错误");
+                    return null;
         }
-        return result;
     }
 
     public static void main(String[] args) {
-        GithubProvider.getAnswer("老头打坐爱唱大悲咒","老头喜欢什么");
+        GithubProvider.getAnswer("我最喜欢吃香蕉","老头最喜欢吃的水果是什么？",1);
     }
 }
