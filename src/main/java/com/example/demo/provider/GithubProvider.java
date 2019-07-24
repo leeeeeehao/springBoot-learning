567ac7d90a589c26a47dc32d8e5f26a6bb0548f3
lihao1998 <1192263018@qq.com>
2019-07-24 14:17:31 +0800

    Test

--- src/main/java/com/example/demo/provider/GithubProvider.java
+++ src/main/java/com/example/demo/provider/GithubProvider.java
@@ -1,11 +1,14 @@
 package com.example.demo.provider;
 
 import com.alibaba.fastjson.JSON;
+import com.example.demo.DemoApplication;
 import com.example.demo.dto.AccessTokenDto;
 import com.example.demo.dto.GithubUser;
 import okhttp3.*;
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  * @author leehao
@@ -62,4 +65,58 @@ public class GithubProvider {
         return null;
     }
 
+    /**
+     * 用户问题回答
+     * @param userDcr,userQuestion
+     * @return result
+     */
+    public static String getAnswer(String userDcr,String userQuestion){
+//        Log.i("TAG","用户话语:" + userDcr + "," + "用户提问:" + userQuestion);
+//        Properties proper = ProperTies.getProperties(ApplicationUtil.getContext());
+        int count = 0;//重复数
+        String result = "";//结果字符串
+//        String exceptionText = proper.getProperty("exceptionText");
+        char[] dcrArray = userDcr.toCharArray(),questionArray = userQuestion.toCharArray();
+        for(int i=0;i<dcrArray.length;i++){
+            for(int j=0;j<questionArray.length;j++){
+                if(dcrArray[i]==questionArray[j]){
+                    result += String.valueOf(dcrArray[i]);
+                    count++;
+                }
+            }
+        }
+        //如果有重复,否则抛出异常答复
+        if(count!=0){
+            count = 0;
+            //将重复字符串从用户话语中删除
+            result = userDcr.replace(result,"");
+            char[] resultArray = result.toCharArray();
+            for(int i=0;i<resultArray.length;i++){
+                for(int j=0;j<questionArray.length;j++){
+                    if(resultArray[i]==questionArray[j]){
+                        count++;
+                    }
+                }
+            }
+            if(count!=0){
+                result = "哈哈换个方式问我吧";
+//                Log.w("TAG","语句多次重复,抛出异常回答:" + exceptionText);
+                System.out.println(result);
+            }
+            else{
+//                Log.i("TAG","成功:" + result);
+                System.out.println(result);
+            }
+        }
+        else{
+//            Log.w("TAG","无重复部分,抛出异常回答:" + exceptionText);
+            result = "哈哈换个方式问我吧";
+            System.out.println(result);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        GithubProvider.getAnswer("老头打坐爱唱大悲咒","老头喜欢什么");
+    }
 }
